862cb8d0006c7e8e7623409dc99533491fbf8ff9
EmilSchier <emil.schier@outlook.com>
2020-10-24 23:39:11 +0200

    Created the framework for an inventory system using Scriptable opjects, still needs UI and some finetuning.

---
+++ Assets/ScriptableObjects/Items/Scripts/ConsumableObject.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(fileName = "New Consumable Object", menuName = "Inventory System/Items/Consumable")]
+public class NewBehaviourScript : ItemObject
+{
+  public int restoredHealthValue;
+  public void Awake()
+  {
+    type = ItemType.Consumable;
+  }
+}
